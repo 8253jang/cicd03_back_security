74b08e7dfe2d2949db89528691d230118aae91aa
BongBong <8253jang@daum.net>
2025-12-10 10:28:39 +0900

    CORS setting

--- src/main/java/web/mvc/config/WebMvcConfig.java
+++ src/main/java/web/mvc/config/WebMvcConfig.java
@@ -19,7 +19,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
      registry.addMapping("/**")
     // .allowedOrigins("http://localhost:5173","http://localhost:4173")
      //.allowedOrigins("http://43.200.184.113:5173", "http://43.200.184.113:4173")
-      .allowedOrigins("http://43.203.170.229", "http://43.203.170.229:80")
+      .allowedOrigins("http://13.125.171.9", "http://13.125.171.9:80")
       .allowedOrigins("http://heejung.n-e.kr", "https://heejung.n-e.kr")
      .allowedMethods("OPTIONS","GET","POST","PUT","DELETE");
   }
