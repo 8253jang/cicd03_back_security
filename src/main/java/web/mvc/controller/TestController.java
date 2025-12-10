9a82c8b14e0df10d2e693b0169022c822e62cc55
BongBong <8253jang@daum.net>
2025-12-10 09:47:04 +0900

    Notification Slack

--- src/main/java/web/mvc/controller/TestController.java
+++ src/main/java/web/mvc/controller/TestController.java
@@ -25,6 +25,6 @@ public class TestController {
         return ResponseEntity
                 .ok()
                 .contentType(MediaType.valueOf("text/plain;charset=UTF-8"))  //
-                .body("CI/CD GitHub Action Slack - spring Security OK 시작! - 오늘은 여기까지~~");
+                .body("CI/CD GitHub Action Slack - spring Security OK 시작! - Notify to Slack on Success~");
     }
 }
